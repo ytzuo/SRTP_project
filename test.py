3ca2e3bec6c662d4e51f23961b4dcadf8e47c308
ytz <1472290813@qq.com>
2025-03-17 21:27:19 +0800

    新增了test.py, .gitignore, requirement.txt

---
+++ test.py
@@ -0,0 +1,22 @@
+import torch
+import torch_geometric
+import pandas
+import numpy
+import einops
+import torch_sparse
+import torch_scatter
+import torchvision
+import sklearn
+import tqdm
+
+#python3.10.12
+print(torch.__version__)
+print(torch_geometric.__version__)
+print(pandas.__version__)
+print(numpy.__version__)
+print(einops.__version__)
+print(torch_sparse.__version__)
+print(torch_scatter.__version__)
+print(torchvision.__version__)
+print(sklearn.__version__)
+print(tqdm.__version__)
